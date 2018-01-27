7703432942d715e2e41218fc3c7c195565341e3f
Jess Bird <jessannbird@gmail.com>
2018-01-27 21:15:12 +0000

    scaffolded alert UX

---
+++ src/components/alert.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import Button from './button'
+import apple from './../assets/apple-heart.jpg'
+import './../css/main.css'
+
+const Alert = (props) => (
+    <div className="alert main">
+      <div className="title">Your heart rate's a little high.</div>
+      <img className="main-img" src={apple} alt="an apple" />
+      <div className="body">Want to save any notes about this?</div>
+      <textarea className="main-form" />
+      <div className="button" onClick={props.closeAlert}><Link to="/">Submit</Link></div>
+    </div>
+)
+
+export default Alert
