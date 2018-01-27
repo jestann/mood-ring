8f32c04b06867b92d5885c3d7fde9cbb9a30f126
Jess Bird <jessannbird@gmail.com>
2018-01-27 21:10:46 +0000

    integrated images into design

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-import registerServiceWorker from './tools/registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/app'
+import registerServiceWorker from './tools/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+)
+
+registerServiceWorker()
