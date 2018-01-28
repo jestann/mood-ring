c5c9a6b2e1cdb934f1bf13390234990fc1180ee8
Jess Bird <jessannbird@gmail.com>
2018-01-28 06:45:18 +0000

    debugging alert functionality

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import Button from './button'
 import apple from './../assets/apple-heart.jpg'
 import './../css/main.css'
 
 const Alert = (props) => (
-    <div className="alert main">
-      <div className="title">Your heart rate's a little high.</div>
-      <img className="main-img" src={apple} alt="an apple" />
-      <div className="body">Want to save any notes about this?</div>
-      <textarea className="main-form" />
-      <div className="button" onClick={props.closeAlert}><Link to="/">Submit</Link></div>
-    </div>
+  <div className="alert main">
+    <div className="title">Your heart rate's a little high.</div>
+    <img className="main-img" src={apple} alt="an apple" />
+    <div className="body">Want to save any notes about this?</div>
+    <textarea 
+      className="main-form" 
+      placeholder="jot down a couple notes about what happened here" 
+      onChange={props.handleNoteChange} 
+    />
+    <div className="button" onClick={props.closeAlert}><Link to="/">Submit</Link></div>
+  </div>
 )
 
 export default Alert
