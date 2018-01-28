bcb97cd21001c9b3fb423b1615a7da839b46f8e4
Jess Bird <jessannbird@gmail.com>
2018-01-28 02:40:38 +0000

    configuring firebase

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import * as firebase from 'firebase'
-import Alert from './alert'
+import fire from './../tools/fire'
 
+import Alert from './alert'
 import Home from './home'
 import Login from './login'
 import NewUser from './newUser'
@@ -19,7 +20,7 @@ class App extends Component {
     super(props)
     this.state = { 
       temps: [],
-      recentRate: 0
+      recentRate: 0,
       loading: true,
       auth: false,
       new: true,
@@ -33,7 +34,7 @@ class App extends Component {
   }
   
   componentWillMount() {
-    this.firebaseRef = firebase.database().ref("temps")
+    let tempsRef = firebase.database().ref("temps")
     this.bindAsArray(this.firebaseRef, "temps")
     /* this.firebaseRef.on("child_added", function(dataSnapshot) {
     this.items.push(dataSnapshot.val())
@@ -42,6 +43,36 @@ class App extends Component {
     })
   }.bind(this)) */
   }
+  
+  componentWillMount(){
+    /* Create reference to messages in Firebase Database */
+    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
+    messagesRef.on('child_added', snapshot => {
+      /* Update React state when message is added at Firebase Database */
+      let message = { text: snapshot.val(), id: snapshot.key };
+      this.setState({ messages: [message].concat(this.state.messages) });
+    })
+  }
+  addMessage(e){
+    e.preventDefault(); // <- prevent form submit from reloading the page
+    /* Send the message to Firebase */
+    fire.database().ref('messages').push( this.inputEl.value );
+    this.inputEl.value = ''; // <- clear the input
+  }
+  render() {
+    return (
+      <form onSubmit={this.addMessage.bind(this)}>
+        <input type="text" ref={ el => this.inputEl = el }/>
+        <input type="submit"/>
+        <ul>
+          { /* Render the list of messages */
+            this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
+          }
+        </ul>
+      </form>
+    );
+  }
+  
 
   componentWillUnmount () {
     this.firebaseRef.off()
