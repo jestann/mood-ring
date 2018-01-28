96ff328997e16bda5609054a203fa896b5723eb6
Jess Bird <jessannbird@gmail.com>
2018-01-28 02:48:08 +0000

    configuring firebase further

--- src/components/app.js
+++ src/components/app.js
@@ -32,47 +32,15 @@ class App extends Component {
     this.setFirebaseNote = this.setFirebaseNote.bind(this)
     this.getFirebaseTemps = this.getFirebaseTemps.bind(this)
   }
-  
+
+  // gets data from Firebase and syncs with state  
   componentWillMount() {
-    let tempsRef = firebase.database().ref("temps")
-    this.bindAsArray(this.firebaseRef, "temps")
-    /* this.firebaseRef.on("child_added", function(dataSnapshot) {
-    this.items.push(dataSnapshot.val())
-    this.setState({
-      items: this.items
-    })
-  }.bind(this)) */
-  }
-  
-  componentWillMount(){
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
-    messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ messages: [message].concat(this.state.messages) });
+    let tempsRef = fire.database().ref("temps").limitToLast(100)
+    tempsRef.on('child_add', snapshot => {
+      let temp = snapshot.val()
+      this.setState({ temps: [temp].concat(this.state.temps) })
     })
   }
-  addMessage(e){
-    e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref('messages').push( this.inputEl.value );
-    this.inputEl.value = ''; // <- clear the input
-  }
-  render() {
-    return (
-      <form onSubmit={this.addMessage.bind(this)}>
-        <input type="text" ref={ el => this.inputEl = el }/>
-        <input type="submit"/>
-        <ul>
-          { /* Render the list of messages */
-            this.state.messages.map( message => <li key={message.id}>{message.text}</li> )
-          }
-        </ul>
-      </form>
-    );
-  }
-  
 
   componentWillUnmount () {
     this.firebaseRef.off()
@@ -86,14 +54,10 @@ class App extends Component {
     this.setState({ alert: false })
   }
   
-  async setFirebaseNote (note) {
-    await this.firebaseRef.push({ note: note })
+  setFirebaseNote (note) {
+    fire.database().ref('temps').push({ note: note })
   }
-  
-  async getFirebaseTemps () {
-    let // call firebase API
-  }
-  
+
   login () {
     // authenticate here
     this.setState({ auth: true })
